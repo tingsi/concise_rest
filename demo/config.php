077311cff0f7ea602b2b050abcfb1bf9035f88a9
z.reddish.j <z.reddish.j@gmail.com>
2023-08-14 15:37:42 +0800

    remove password in code

--- demo/config.php
+++ demo/config.php
@@ -3,8 +3,8 @@
 
 $GLOBALS['config']['db']['db_host'] = 'localhost';
 $GLOBALS['config']['db']['db_port'] = 3306;
-$GLOBALS['config']['db']['db_user'] = 'wang';
-$GLOBALS['config']['db']['db_password'] = 'hongliang';
+$GLOBALS['config']['db']['db_user'] = '<youdbname>';
+$GLOBALS['config']['db']['db_password'] = '<youdbpasswd>';
 $GLOBALS['config']['db']['db_name'] = 'proj';
 
 $GLOBALS['config']['sys']['token_days'] = 7;    # 7days
